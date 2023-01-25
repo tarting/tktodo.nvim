f85c0fc1e20bfc21a1ae95b301f188d58ae2c301
Trygvi Bech Árting <trygvi@barting.xyz>
2023-01-26 00:57:51 +0100

    it works

--- lua/telescope/_extensions/tktodo.lua
+++ lua/telescope/_extensions/tktodo.lua
@@ -7,28 +7,125 @@ local sorters = require("telescope.sorters")
 local make_entry = require("telescope.make_entry")
 local api = vim.api
 
+
+
+------------------------------------------------------------------------------------
+-- begin https://stackoverflow.com/a/43582076
+
+-- gsplit: iterate over substrings in a string separated by a pattern
+-- 
+-- Parameters:
+-- text (string)    - the string to iterate over
+-- pattern (string) - the separator pattern
+-- plain (boolean)  - if true (or truthy), pattern is interpreted as a plain
+--                    string, not a Lua pattern
+-- 
+-- Returns: iterator
+--
+-- Usage:
+-- for substr in gsplit(text, pattern, plain) do
+--   doSomething(substr)
+-- end
+local function gsplit(text, pattern, plain)
+  local splitStart, length = 1, #text
+  return function ()
+    if splitStart then
+      local sepStart, sepEnd = string.find(text, pattern, splitStart, plain)
+      local ret
+      if not sepStart then
+        ret = string.sub(text, splitStart)
+        splitStart = nil
+      elseif sepEnd < sepStart then
+        -- Empty separator!
+        ret = string.sub(text, splitStart, sepStart)
+        if sepStart < length then
+          splitStart = sepStart + 1
+        else
+          splitStart = nil
+        end
+      else
+        ret = sepStart > splitStart and string.sub(text, splitStart, sepStart - 1) or ''
+        splitStart = sepEnd + 1
+      end
+      return ret
+    end
+  end
+end
+
+-- split: split a string into substrings separated by a pattern.
+-- 
+-- Parameters:
+-- text (string)    - the string to iterate over
+-- pattern (string) - the separator pattern
+-- plain (boolean)  - if true (or truthy), pattern is interpreted as a plain
+--                    string, not a Lua pattern
+-- 
+-- Returns: table (a sequence table containing the substrings)
+local function split(text, pattern, plain)
+  local ret = {}
+  for match in gsplit(text, pattern, plain) do
+    table.insert(ret, match)
+  end
+  return ret
+end
+
+-- end https://stackoverflow.com/a/43582076
+--------------------------------------------------------------------------------
+
+
 local M = {}
 local tk_conf = require("telekasten").Cfg
 
+local todo_picker = function(opts)
+    local command = {'grep', '-rHn', '\\- \\[[ x]\\]', opts.notes_home }
+    return pickers.new(opts, {
+        prompt_title = "Todo",
+        finder = finders.new_oneshot_job(command, opts),
+        sorter = sorters.get_generic_fuzzy_sorter(),
+
+        attach_mappings = function(prompt_bufnr, map)
+            local toggle_todo = function()
+                local picker = action_state.get_current_picker(prompt_bufnr)
+                local selections = picker:get_multi_selection()
+                if next(selections) == nil then
+                    selections = {picker:get_selection()}
+                end
+
+                actions.close(prompt_bufnr)
+
+                local tasks = {}
+                for _, t in ipairs(selections) do
+                    table.insert(tasks, t[1])
+                    local tsk = split(t[1],':',true)
+                    -- command = string.format([[sed -i '%ss/- \[ \]/- [x]/' "%s"]],tsk[2], tsk[1])
+                    local replace_command = string.format([[awk -i inplace '{ if (NR==%s) { if (match(" \- \[ \]", $0)) { sub(/ \- \[ \]/," - [x]", $0); print $0 } else { gsub(/ \- \[x\]/," - [ ]", $0); print $0 } } else { print $0} }' %s]], tsk[2], tsk[1])
+                    io.popen(replace_command)
+                end
+            end
+
+            map('i', '<CR>', toggle_todo)
+            map('n', '<CR>', toggle_todo)
+
+            return true
+        end,
+    })
+end
+
+local find_todo = function(opts)
+    opts = opts or {}
+    opts.cwd = opts.cwd or vim.fn.getcwd()
+    opts.notes_home = opts.notes_home or tk_conf.home
+
+    local tdpicker = todo_picker(opts)
+    tdpicker:find()
+end
 
 return require("telescope").register_extension {
     exports = {
         todo = function(opts)
-            opts = opts or {}
-            opts.cwd = opts.cwd or vim.fn.getcwd()
-            opts.notes_home = opts.notes_home or tk_conf.home
-
-            local command = {"LWD=$(pwd) cd ~/notes; find . -iname '*.md' -print0 | xargs -0 grep -Hn '\\- \\[[ x]\\]'; cd $LWD"}
-
-
-            pickers.new(opts, {
-                prompt_title = "Todo",
-                finder = finders.new_oneshot_job(command, opts),
-                sorter = sorters.get_generic_fuzzy_sorter(),
-                attach_mappings = function(func, map)
-                    return 1
-                end,
-            }):find()
+            find_todo(opts)
         end
     }
 }
+
+
